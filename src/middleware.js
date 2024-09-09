c856a30c0395f952071c9262fc2bd1631d02e38c
JonelBriones <jonel.c.briones@gmail.com>
2024-09-09 09:48:10 -0700

    reverting last updates

--- src/middleware.js
+++ src/middleware.js
@@ -2,6 +2,6 @@ export { default } from "next-auth/middleware";
 
 // specify routes to protect that requires auth
 export const config = {
-  // matcher: ["/create-tweet", "/profile", "/activity"],
+  matcher: ["/create-tweet", "/profile", "/activity"],
   // matcher: ["/", "/create-tweet", "/activity", "/search",'/profile'],
 };
